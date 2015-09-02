34498935e821da1508b38326796507f1385ced38
Katherine De La Cruz <katherine.delacruz8@gmail.com>
2015-09-02 18:26:02 -0500

    Add styles to arrow

--- app/assets/javascripts/nav-menu.js
+++ app/assets/javascripts/nav-menu.js
@@ -4,8 +4,10 @@ $(document).ready(function () {
     for (var i = 0; i < $("section .menu").length; i++) {
       $("section .menu").removeClass("show");
       $(".category li").removeClass("active");
+      $("li div").removeClass("arrow");
     }
     $(this).addClass("active");
+    $(this).find("div").addClass("arrow");
     $('#' + name).addClass("show");
   });
 });
